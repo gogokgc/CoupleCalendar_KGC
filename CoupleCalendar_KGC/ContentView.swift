2c5c9f98f2567e27907f173263e58db05c6cbcdf
gogokgc <gogokgc@gmail.com>
2024-05-15 10:08:33 +0900

    기초코드 설명 주석 추가

--- CoupleCalendar_KGC/ContentView.swift
+++ CoupleCalendar_KGC/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    ///CoreData의 관리 객체 컨텍스트를 가져옵니다. 이 컨텍스트를 사용하여 CoreData의 데이터를 관리합니다.
     @Environment(\.managedObjectContext) private var viewContext
 
+    ///CoreData에서 데이터를 가져오기 위한 속성입니다. Item 엔터티를 기준으로 정렬된 데이터를 가져옵니다.
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
@@ -33,6 +35,7 @@ struct ContentView: View {
                     EditButton()
                 }
                 ToolbarItem {
+                    
                     Button(action: addItem) {
                         Label("Add Item", systemImage: "plus")
                     }
@@ -41,7 +44,8 @@ struct ContentView: View {
             Text("Select an item")
         }
     }
-
+    
+    ///데이터를 추가하는 메서드입니다. CoreData에 새로운 Item 객체를 추가하고, 관리 객체 컨텍스트를 저장합니다.
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
@@ -58,6 +62,7 @@ struct ContentView: View {
         }
     }
 
+    ///데이터를 삭제하는 메서드입니다. 선택된 인덱스에 해당하는 아이템을 CoreData에서 삭제하고, 관리 객체 컨텍스트를 저장합니다.
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
@@ -81,6 +86,7 @@ private let itemFormatter: DateFormatter = {
     return formatter
 }()
 
+///SwiftUI 뷰의 미리보기를 정의합니다. PersistenceController.preview.container.viewContext를 환경에 제공하여 CoreData의 미리보기용 컨텍스트를 사용합니다.
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
