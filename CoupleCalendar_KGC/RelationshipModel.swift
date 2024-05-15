63ed0b7df6bce5e17669024cc705a376763a3634
gogokgc <gogokgc@gmail.com>
2024-05-15 10:09:14 +0900

    모델 정의 날짜와 메모 추가

--- CoupleCalendar_KGC/RelationshipModel.swift
+++ CoupleCalendar_KGC/RelationshipModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import CoreData
+
+public class Relationship: NSManagedObject, Identifiable {
+    @NSManaged public var startDate: Date
+    @NSManaged public var memos: NSSet?
+}
+
+extension Relationship {
+    static func getAllRelationships() -> NSFetchRequest<Relationship> {
+        let request: NSFetchRequest<Relationship> = Relationship.fetchRequest() as! NSFetchRequest<Relationship>
+        request.sortDescriptors = [NSSortDescriptor(key: "startDate", ascending: true)]
+        return request
+    }
+}
